537580d2677b69a6237018cb302bf253be11fdc8
Lucas Avila <lucasraavila@outlook.com>
2024-01-24 23:11:57 -0300

    tudo

---
+++ Aula6/script.js
@@ -0,0 +1,34 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter Lucas</h1>`;
+
+
+// declarar variavel
+// var  private/public
+//let local
+// const não altera
+var sobrenome;
+let nome;
+const idade = 33;
+let hist;
+
+
+nome = "Lucas Ávila";
+console.log(nome);
+console.log(idade);
+sobrenome = "Ávila"
+console.log(sobrenome)
+
+nome=1;
+console.log(nome)
+
+// abre janela para receber um valor 
+//hist = prompt("Digite algo")
+ 
+console.log(hist)
+
+//pop de msg
+//alert(hist)
